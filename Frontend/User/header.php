0aa822ad867244a334529208f5027596963e53e3
harmeetsingh01 <2018.harmeet.kathoda@ves.ac.in>
2020-10-28 16:49:51 +0530

    Header Template & css

---
+++ Frontend/User/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet"> -->
+    <link rel="stylesheet" href="../Css/header.css">
+    <link rel="stylesheet" href="../Css/itemProfile.css">
+    <link rel="stylesheet" href="../Css/menu.css">
+
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <header>
+        <h2>Zwiggy</h2>
+        <ul class="nav">
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">Reservations</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Cart</a></li>
+        </ul>
+
+        <button class="logout">Log Out</button>
+    </header>
